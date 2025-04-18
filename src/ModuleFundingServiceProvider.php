6df261967d9b9dc4369f6e1aed88ec415df16374
Hamzah <hamzahnafalah@gmail.com>
2025-04-18 18:30:42 +0700

    add package generator module

--- src/ModuleFundingServiceProvider.php
+++ src/ModuleFundingServiceProvider.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Hanafalah\ModuleFunding;
 
 use Hanafalah\LaravelSupport\Providers\BaseServiceProvider;
-use Hanafalah\ModuleFunding\Schemas\Funding;
 
 class ModuleFundingServiceProvider extends BaseServiceProvider
 {
