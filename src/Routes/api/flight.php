0457c419445202bfe6f897c71acce725ad7c420c
Hamzah <hamzahnafalah@gmail.com>
2025-04-29 22:46:20 +0700

    update modules

--- src/Routes/api/flight.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Hanafalah\ModuleFunding\Controllers\API\Flight\FlightController;
-use Illuminate\Support\Facades\Route;
-
-Route::apiResource('flight', FlightController::class)
-    ->parameters(['flight' => 'id']);
